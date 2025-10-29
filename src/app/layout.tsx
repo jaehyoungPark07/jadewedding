b0fcc3e9432ff58ee6e54350b378e484b4d3127e
박재형 <jaehyoung@bagjaehyeong-ui-MacBookAir.local>
2025-10-29 17:41:59 +0900

    image edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,34 @@
-import type { Metadata, Viewport } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.scss';
-import Script from 'next/script';
-import styles from './layout.module.scss';
+import type { Metadata, Viewport } from "next";
+import { Inter } from "next/font/google";
+import "./globals.scss";
+import Script from "next/script";
+import styles from "./layout.module.scss";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: '박진원 ♡ 강현미 결혼 합니다.',
-  description: '박진원 ♡ 강현미 결혼 합니다.',
+  title: "박진원 ♡ 강현미 결혼 합니다.",
+  description: "박진원 ♡ 강현미 결혼 합니다.",
   openGraph: {
-    title: '박진원 ♡ 강현미 결혼 합니다.',
-    description: '박진원 ♡ 강현미 결혼 합니다.',
+    title: "박진원 ♡ 강현미 결혼 합니다.",
+    description: "박진원 ♡ 강현미 결혼 합니다.",
+    images: [
+      {
+        url: "/img/웨딩메인.jpeg",
+        width: 800,
+        height: 600,
+      },
+    ],
   },
 };
 
 export const viewport: Viewport = {
-  width: 'device-width',
+  width: "device-width",
   initialScale: 1,
   maximumScale: 1,
   minimumScale: 1,
   userScalable: false,
-  viewportFit: 'cover',
+  viewportFit: "cover",
 };
 
 export default function RootLayout({
@@ -32,10 +39,10 @@ export default function RootLayout({
   const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=d5b38a623bd2e216765a0660ce1537d1&autoload=false`;
 
   return (
-    <html lang='ko'>
+    <html lang="ko">
       <head>
-        <Script src={KAKAO_SDK_URL} strategy='beforeInteractive' />
-        <meta property='og:image' content='/img/logo.png' />
+        <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
+        <meta property="og:image" content="/img/logo.png" />
       </head>
       <body className={`${inter.className} ${styles.container}`}>
         {children}
